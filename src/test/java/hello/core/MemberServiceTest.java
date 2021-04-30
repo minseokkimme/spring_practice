1721ad8580e66dfb7874f54ab72de4af60f41956
minseokkimme <minseok0818@naver.com>
2021-04-30 15:15:40 +0900

    test : Add MemberServiceTest class 멤버를 생성한 후  join한 member과  find 함수를 쓴 member가 같은지 확인하는 테스트 추가

---
+++ src/test/java/hello/core/MemberServiceTest.java
@@ -0,0 +1,24 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+
+    @Test
+    void join(){
+
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+}
